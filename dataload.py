e3cdecd860f2b41e8617a85bab2421f4ad23f06b
진원 <jinwongim598@gmail.com>
2024-05-22 15:39:07 +0900

    Update dataload.py

--- dataload.py
+++ dataload.py
@@ -1,14 +1,17 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import pydeck as pdk
+import plotly.express as px
 
 st.title('Uber pickups in NYC')
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
 # 데이터 불러오기
+@st.cache
 def load_data(nrows):
     data = pd.read_csv(DATA_URL, nrows=nrows)
     lowercase = lambda x: str(x).lower()
@@ -29,3 +32,52 @@ data_load_state.text('Loading data...done!')
 # 부제목 만들기
 st.subheader('Raw data')
 st.write(data)
+
+# 히스토그램 추가
+st.subheader('Number of pickups by hour')
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
+
+# 지도 시각화 추가
+st.subheader('Map of all pickups')
+st.map(data[['latitude', 'longitude']])
+
+# 특정 시간대 필터링
+hour_to_filter = st.slider('hour', 0, 23, 17)  # 기본적으로 17시를 선택
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+
+st.subheader(f'Map of all pickups at {hour_to_filter}:00')
+st.map(filtered_data[['latitude', 'longitude']])
+
+# pydeck을 사용한 고급 지도 시각화
+st.subheader('Pickups by location')
+midpoint = (np.average(data['latitude']), np.average(data['longitude']))
+
+st.pydeck_chart(pdk.Deck(
+    map_style='mapbox://styles/mapbox/light-v9',
+    initial_view_state=pdk.ViewState(
+        latitude=midpoint[0],
+        longitude=midpoint[1],
+        zoom=11,
+        pitch=50,
+    ),
+    layers=[
+        pdk.Layer(
+            'HexagonLayer',
+            data=data,
+            get_position='[longitude, latitude]',
+            radius=100,
+            elevation_scale=4,
+            elevation_range=[0, 1000],
+            pickable=True,
+            extruded=True,
+        ),
+        pdk.Layer(
+            'ScatterplotLayer',
+            data=data,
+            get_position='[longitude, latitude]',
+            get_color='[200, 30, 0, 160]',
+            get_radius=200,
+        ),
+    ],
+))
