c8ba989e56e40051f21470dd7d5bb55244fb5868
진원 <jinwongim598@gmail.com>
2024-05-22 15:44:23 +0900

    Update dataload.py

--- dataload.py
+++ dataload.py
@@ -9,13 +9,17 @@ DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
             'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
 # 데이터 불러오기
-@st.cache_data
+@st.cache
 def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
+    try:
+        data = pd.read_csv(DATA_URL, nrows=nrows)
+        lowercase = lambda x: str(x).lower()
+        data.rename(lowercase, axis='columns', inplace=True)
+        data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+        return data
+    except Exception as e:
+        st.error(f"Error loading data: {e}")
+        return pd.DataFrame()
 
 # 텍스트 요소 생성. 사용자에게 데이터가 로드 되고 있음을 알린다.
 data_load_state = st.text('Loading data...')
@@ -24,24 +28,27 @@ data_load_state = st.text('Loading data...')
 data = load_data(10000)
 
 # 데이터가 성공적으로 로드 되었음을 알린다.
-data_load_state.text('Loading data...done!')
+if not data.empty:
+    data_load_state.text('Loading data...done!')
 
-# 부제목 만들기
-st.subheader('Raw data')
-st.write(data)
+    # 부제목 만들기
+    st.subheader('Raw data')
+    st.write(data)
 
-# 히스토그램 추가
-st.subheader('Number of pickups by hour')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0, 24))[0]
-st.bar_chart(hist_values)
+    # 히스토그램 추가
+    st.subheader('Number of pickups by hour')
+    hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0, 24))[0]
+    st.bar_chart(hist_values)
 
-# 지도 시각화 추가
-st.subheader('Map of all pickups')
-st.map(data[['latitude', 'longitude']])
+    # 지도 시각화 추가
+    st.subheader('Map of all pickups')
+    st.map(data[['lat', 'lon']])
 
-# 특정 시간대 필터링
-hour_to_filter = st.slider('hour', 0, 23, 17)  # 기본적으로 17시를 선택
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+    # 특정 시간대 필터링
+    hour_to_filter = st.slider('hour', 0, 23, 17)  # 기본적으로 17시를 선택
+    filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 
-st.subheader(f'Map of all pickups at {hour_to_filter}:00')
-st.map(filtered_data[['latitude', 'longitude']])
+    st.subheader(f'Map of all pickups at {hour_to_filter}:00')
+    st.map(filtered_data[['lat', 'lon']])
+else:
+    data_load_state.text('Loading data...failed!')
